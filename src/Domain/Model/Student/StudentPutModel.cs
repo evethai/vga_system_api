06384bdb437540843679ae87b1f82fdd183e72b2
BlackerDK <duykhanhz1000st@gmail.com>
2024-10-05 21:11:44 +0700

    fix: valid field student and highschool

--- src/Domain/Model/Student/StudentPutModel.cs
+++ src/Domain/Model/Student/StudentPutModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ using System.Threading.Tasks;
 namespace Domain.Model.Student;
 public class StudentPutModel
 {
+    [Required(ErrorMessage = "Status is required.")]
     public bool Status { get; set; }   
 }
