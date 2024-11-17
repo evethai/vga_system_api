ebb27424e48bfe47c7069a63c5ee51409698ffe2
GnuDNaT <vongoctandung2002@gmail.com>
2024-11-17 21:10:21 +0700

    fix: add image field to occupation group model

--- src/Domain/Model/OccupationalGroup/OccupationalGroupPutModel.cs
+++ src/Domain/Model/OccupationalGroup/OccupationalGroupPutModel.cs
@@ -10,5 +10,6 @@ namespace Domain.Model.OccupationalGroup
     {
         public string? Name { get; set; } 
         public string? Description { get; set; }
+        public string? Image { get; set; } = string.Empty;
     }
 }
