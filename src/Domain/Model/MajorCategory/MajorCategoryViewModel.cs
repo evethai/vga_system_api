9d43fc486114b53b75cf57c15a50cdbc1173ad9a
GnuDNaT <vongoctandung2002@gmail.com>
2024-11-18 12:28:49 +0700

    fix: add des field in major category, modify major category model

--- src/Domain/Model/MajorCategory/MajorCategoryViewModel.cs
+++ src/Domain/Model/MajorCategory/MajorCategoryViewModel.cs
@@ -12,5 +12,6 @@ namespace Domain.Model.MajorCategory
         public string Name { get; set; } = string.Empty;
         public bool Status { get; set; }
         public string Image { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
